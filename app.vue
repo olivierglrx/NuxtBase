47ea6e6dbeb16309844cb0ba068182cff08970a0
olivierglrx <olivier.glrx@gmail.com>
2024-03-09 13:15:22 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<style>
+html {
+  scroll-behavior: smooth;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+import Navbar from '~/components/Navbar.vue';
+<template>
+  <div>
+    <Navbar></Navbar>
+  </div>
+</template>
