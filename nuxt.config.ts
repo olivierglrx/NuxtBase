da9cd156eb6ffbdfc10a0a73e42d0fccf004aef3
olivierglrx <olivier.glrx@gmail.com>
2024-03-12 13:42:24 +0100

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-
-})
+  modules: ["@nuxt/ui", "nuxt-primevue"],
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
+        },
+      ],
+    },
+  },
+});
